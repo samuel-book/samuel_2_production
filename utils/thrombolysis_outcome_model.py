fa129cb498f3e3f05e049f09441a008230c4bdaa
Michael Allen <michael.allen1966@gmail.com>
2023-12-06 15:41:14 +0000

    Add multiclass model train and test

--- utils/thrombolysis_outcome_model.py
+++ utils/thrombolysis_outcome_model.py
@@ -4,8 +4,8 @@ import pandas as pd
 import pickle
 import shap
 
-from sklearn.metrics import auc
-from sklearn.metrics import roc_curve
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_auc_score
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
@@ -74,4 +74,61 @@ class OutcomeModel():
 
         self.X_test.to_csv('./output/thrombolysis_outcome_feature_values.csv')
 
+    
+    def run(self):
+        """
+        Train model, get SHAP values, and estimate benchmark thrombolysis rates
+        """
 
+        self.train_model()
+        self.test_model()
+
+    
+    def test_model(self):
+
+        """
+        Test model
+        """
+
+        # Get predicted probabilities
+        y_probs = self.model.predict_proba(self.X_test_one_hot)
+        y_pred = self.model.predict(self.X_test_one_hot)
+
+        # Plot confusion matrix
+        cm = confusion_matrix(self.y_test.astype(np.int8), y_pred)
+        fig = plt.figure(figsize=(6,6))
+        ax1 = fig.add_subplot(1,1,1)
+        heatmap = plt.imshow(cm, cmap=plt.cm.Blues)
+        colorbar = plt.colorbar(heatmap, shrink=0.8, ax=ax1, alpha=0.5, label='Count')
+        # To add values to plot
+        for i in range(len(cm)):
+            for j in range(len(cm[0])):
+                plt.text(i, j, cm[i][j], ha='center', va='center', color='r')
+        ax1.set_xlabel('Predicted value')
+        ax1.set_ylabel('Observed value')
+        # Save
+        plt.savefig('./output/thrombolysis_outcome_confusion_matrix.png',
+                    dpi=300, bbox_inches='tight')
+        plt.close()
+
+        # Get AUC
+        auc = roc_auc_score(self.y_test.astype(np.int8), y_probs,
+                            multi_class='ovo', average='macro') 
+        
+        print(f'Outcome multiclass ROC AUC {auc:.3f}')
+
+
+    
+    def train_model(self):
+
+        """
+        Train model, get SHAP values, and estimate benchmark thrombolysis rates
+        """
+
+        # Define and Fit model
+        self.model = XGBClassifier(verbosity=0, seed=42)
+        self.model.fit(self.X_train_one_hot, self.y_train)
+
+        # Pickle model and save to pickled_models folder
+        pickle.dump(self.model, open('./pickled_models/thrombolysis_choice_model.pkl', 'wb'))
+    
